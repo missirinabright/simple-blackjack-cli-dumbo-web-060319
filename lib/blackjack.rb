4c6301fe2f4ef5a77d59730191f34d25ff3462eb
Irina Bright <missirinabright@gmail.com>
2019-05-21 16:07:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -72,8 +72,8 @@ end
 def runner
   welcome
   initial_round
-until total > 21
-      total = hit?(card_total)
+until card_total > 21
+      card_total = hit?(card_total)
       display_card_total(card_total)
     end
 end_game(card_total)
